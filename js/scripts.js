4288c9ea8f5e8915949c8e8652dbfef1696f4cbf
melissa-koi <melissawangui3@gmail.com>
2021-02-28 19:37:23 +0300

    add the toggle function

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".clickable1").click(function(){
+        $(".messShowing1").slideToggle();
+        $(".messHidden1").slideToggle();
+    });
+    $(".clickable2").click(function(){
+        $(".messShowing2").slideToggle();
+        $(".messHidden2").slideToggle();
+    });
+    $(".clickable3").click(function(){
+        $(".messShowing3").slideToggle();
+        $(".messHidden3").slideToggle();
+    });
+});
