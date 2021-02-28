3a7a2e6d08ae9902fb514258eaacf0ef828fa55f
melissa-koi <melissawangui3@gmail.com>
2021-03-01 01:36:44 +0300

    complete contact us

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,22 @@ $(document).ready(function(){
         $(".messHidden3").slideToggle();
     });
 
-    let myForm=$("#myForm")
-    console.log(myForm)
 });
+function validate() {
+    let myForm = $("#myForm")
+    let names = $("#name").val();
+    let email = $("#email").val();
+    let message = $("#message").val();
+
+    if(names == '' || email == '' || message == '') {
+        alert("Please fill in all the fields")
+        return false;
+    }else{
+        let link="https://gmail.us1.list-manage.com/subscribe/post?u=70d5d821ac16afadc7dce3223&amp;id=6eb95e58bc"
+        myForm.attr('action',link)
+        myForm.attr('method' ,'post')
+        let response = `${names} ,we have received your message. Thank you for reaching out to us!`
+        alert(response)
+    }
+}
+
